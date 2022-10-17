dfe0137b20eb42ab1be6abffc70f2f95c3f93f85
k0ldy <114028346+k0ldy@users.noreply.github.com>
2022-10-18 00:20:47 +0300

    Update laba5zadacha1.py

--- laba5zadacha1.py
+++ laba5zadacha1.py
@@ -2,3 +2,13 @@
 a = [1,2,3]
 b = "xyz"
 c = (None, True)
+
+res = list(zip(a, b, c))
+print (res)
+
+# reduce()
+from functools import reduce
+items = [1, 24, 17, 14, 9, 32, 2]
+all_max = reduce(lambda a,b: a if (a > b) else b, items)
+
+print (all_max)
